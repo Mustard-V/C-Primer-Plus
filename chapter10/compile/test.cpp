bc8a7339aa28c536db55070cec5324c96cced80e
Zishuo Li <l1277202997@gmail.com>
2020-12-23 01:16:39 +0800

    something very strange in mingw32

---
+++ chapter10/compile/test.cpp
@@ -0,0 +1,19 @@
+//test.cpp
+#include "test.h"
+#include <iostream>
+using namespace std;
+
+Test::Test()
+{
+
+}
+
+void Test::hello()
+{
+    cout << "hello" << endl;
+}
+
+Test::~Test()
+{
+
+}
---
+++ chapter10/compile/test.h
@@ -0,0 +1,12 @@
+//test.h
+#ifndef _TEST_H_ 
+#define _TEST_H_ 
+
+class Test
+{
+public:
+    Test();
+    void hello();
+    ~Test();
+};
+#endif  //TEST
