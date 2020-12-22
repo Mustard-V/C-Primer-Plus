310b8586a015d65f1c4f070e7b7cd34c00261534
Zishuo Li <l1277202997@gmail.com>
2020-12-22 21:11:14 +0800

    class for chapter10

---
+++ chapter10/Stock/stock00.h
@@ -0,0 +1,23 @@
+//head for stock
+#ifndef _STOCK_
+#define _STOCK_
+
+#include <string>
+class stock
+{
+private:
+    /* data */
+    std::string compay;
+    long shares;
+    double share_val;
+    double total_val;
+    inline void set_tot() {total_val = share_val*shares;};
+public:
+    void acquire(const std::string & co,long n, double pr);
+    void buy(long num,double price);
+    void sell(long num,double price);
+    void update(double price);
+    void show();
+};
+
+#endif
--- chapter9/self_exe/multi_file_const/header/test.h
+++ chapter9/self_exe/multi_file_const/header/test.h
@@ -1,4 +1,5 @@
 #ifndef TEST_H_
+#define TEST_H_
 using namespace std;
 int what_is_i(int i);
 #endif
