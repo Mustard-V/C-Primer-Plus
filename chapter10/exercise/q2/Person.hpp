131148ec1759c0f74f2dc3f2b4d3d41b62dff2bc
Zishuo Li <l1277202997@gmail.com>
2020-12-25 18:37:07 +0800

    q2

---
+++ chapter10/exercise/q2/Person.hpp
@@ -0,0 +1,16 @@
+#ifndef _PERSON_
+#define _PERSON_
+#include <string>
+class Person {
+    private:
+        static const int LIMIT = 25;
+        std::string lname;
+        char fname[LIMIT];
+    public:
+        Person();
+        Person(const std::string &ln, const char * fn = "Heyyou");
+        ~Person();
+        void Show() const;
+        void FormalShow() const;
+};
+#endif
